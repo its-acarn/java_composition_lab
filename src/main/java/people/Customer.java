b994a3f86ad9df2775c7141a2927dfdcc6c27f42
svvishjs <andreww92@live.com>
2020-11-25 13:58:48 +0000

    added names to dealership and customer classes

--- src/main/java/people/Customer.java
+++ src/main/java/people/Customer.java
@@ -6,12 +6,19 @@ import java.util.ArrayList;
 
 public class Customer {
 
+
+    private String name;
     private double money;
     private ArrayList<Vehicle> ownedVehicles;
 
-    public Customer(double money) {
+    public Customer(String name, double money) {
+        this.name = name;
         this.money = money;
         this.ownedVehicles = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
     }
 
     public double getMoney() {
--- src/main/java/people/Dealership.java
+++ src/main/java/people/Dealership.java
@@ -6,12 +6,18 @@ import java.util.ArrayList;
 
 public class Dealership {
 
+    private String name;
     private ArrayList<Vehicle> stock;
     private double till;
 
-    public Dealership(double till) {
+    public Dealership(String name, double till) {
+        this.name = name;
         this.stock = new ArrayList<>();
         this.till = till;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public ArrayList<Vehicle> getStock() {
